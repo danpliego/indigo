12eadf184f3b9e20a0d9d3424b6565a0359c1340
El dano <danielpliego@gmail.com>
2020-02-27 09:14:36 -0600

    fix build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/danielpliego/projects/indigo/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/danielpliego/projects/indigo/src/pages/404.js"))),
   "component---src-pages-html-js": hot(preferDefault(require("/Users/danielpliego/projects/indigo/src/pages/html.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/danielpliego/projects/indigo/src/pages/index.js")))
